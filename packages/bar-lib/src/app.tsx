9439cf0e669c78de7775ad3223a5a3a4b80af4fc
Stephanie Yu <styu314@gmail.com>
2024-08-27 16:43:54 -0400

    prettier

--- packages/bar-lib/src/app.tsx
+++ packages/bar-lib/src/app.tsx
@@ -2,7 +2,5 @@ import React from "react";
 import { Header } from "@example/foo-lib";
 
 export const App: React.FC = () => {
-    return (
-        <Header title="Hello world"></Header>
-    )
-}
+    return <Header title="Hello world"></Header>;
+};
